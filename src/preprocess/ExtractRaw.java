7d9417040b5149580ed0f780c77fcf7de3a2d96f
xiaohe27 <hexiao2@illinois.edu>
2015-02-26 22:45:50 -0600

    modify

--- src/preprocess/ExtractRaw.java
+++ src/preprocess/ExtractRaw.java
@@ -8,23 +8,55 @@ import java.nio.file.Paths;
  * Created by hx312 on 26/02/2015.
  */
 public class ExtractRaw {
-    public static String getRawMonitoringCode(String rawSpec) throws IOException {
-        String rawCodeUtilLast = rawSpec.substring(rawSpec.indexOf("raw:") + 4);
-        int balanceOfParenthesis = 0, i = 0;
-        for (; i < rawCodeUtilLast.length(); i++) {
-            char curC = rawCodeUtilLast.charAt(i);
-            if (curC == '{')
-                balanceOfParenthesis++;
-            else if (curC == '}')
-                balanceOfParenthesis--;
-            else {}
+           /**
+     * Get the data structure which maps the position in file to raw monitoring code.
+     * There might be multiple specs in single file, as a result, there might be
+     * more than one raw monitoring block in one spec file.
+     * @param rawSpecFile which may contain multiple spec.
+     * @return The map between the position and the monitoring code.
+     * @throws IOException If the monitoring code is not extracted successfully.
+     */
+    private static HashMap<Integer,String> getRawMonitoringCode(String rawSpecFile) throws IOException {
+        HashMap<Integer,String> map = new HashMap<>();
+        int offset = 0;
 
-            if (balanceOfParenthesis == -1) {
-                return rawCodeUtilLast.substring(0,i);
+        while (offset <= rawSpecFile.length()){
+            int balanceOfParenthesis = 0;
+
+            int indexOfRawKeyword = rawSpecFile.indexOf("raw:", offset);
+            if (indexOfRawKeyword == -1)
+                return map;
+
+            offset = indexOfRawKeyword + 4;
+            String rawCodeUtilLast = rawSpecFile.substring(offset);
+            for (int j = 0; j < rawCodeUtilLast.length(); j++, offset++) {
+                char curC = rawCodeUtilLast.charAt(j);
+                if (curC == '{')
+                    balanceOfParenthesis++;
+                else if (curC == '}')
+                    balanceOfParenthesis--;
+                else {
+                }
+
+                if (balanceOfParenthesis == -1) {
+                    map.put(indexOfRawKeyword, rawCodeUtilLast.substring(0, j));
+                    break;
+                }
+            }
+
+            if (offset >= rawSpecFile.length()) {
+                if (balanceOfParenthesis == -1) {
+                    return map;
+                }
+                else {throw new IOException("Unexpected end of file while reading" +
+                        " raw monitoring code.");}
             }
         }
-        throw new IOException("Unexpected end of file while extracting raw monitoring code.");
+
+        return map;
     }
+   
+
 
     public static void main(String[] args) throws IOException {
         String rvmSpec = new String(Files.readAllBytes(Paths.get("HasNext.rvm")));
